00fcae18cc37344c9e6668289c401f4a73a64e03
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-04-25 18:48:51 -0500

    Create simple form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <button type="submit">Save</button>
+      </form>
+    );
   }
 }
 
