ce6f0a5d059cc79ef11f055a22e38290fcd1fc3e
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-04-25 19:32:18 -0500

    Create store and middleware enhancers

---
+++ src/redux/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers/index";
+import { reduxImmutableStateInvariant } from "redux-immutable-state-invariant";
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  // reduxImmutableStateInvariant warns if accidentally mutate redux state
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
+}
