0f99e90111f57d80e4a47dfbb144dc82f147a480
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-05-09 16:16:15 -0500

    Implement object form of mapDispatchToProps

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -3,21 +3,20 @@ import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 
 class ManageCoursePage extends React.Component {
   componentDidMount() {
-    const { courses, authors, actions } = this.props;
+    const { courses, authors, loadCourses, loadAuthors } = this.props;
 
     // checking courses.length and authors.length helps to avoid making unnecessary API class each time page loads
     if (courses.length === 0) {
-      actions.loadCourses().catch((error) => {
+      loadCourses().catch((error) => {
         alert("Loading courses failed " + error);
       });
     }
 
     if (authors.length === 0) {
-      actions.loadAuthors().catch((error) => {
+      loadAuthors().catch((error) => {
         alert("Loading authors failed " + error);
       });
     }
@@ -35,15 +34,13 @@ class ManageCoursePage extends React.Component {
 ManageCoursePage.propTypes = {
   courses: PropTypes.array.isRequired,
   authors: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
 };
 
 // this function determines what state is passed to our component via props
 function mapStateToProps(state) {
-  // FLOW:
-  // 1 when entering the page, courses is empty
-  // 5 finally the state contains the course that was added by the reducer
-  //debugger;
   return {
     courses: state.courses,
     authors: state.authors
@@ -51,19 +48,11 @@ function mapStateToProps(state) {
 }
 
 // this function determines what actions are available on props in our component
-function mapDispatchToProps(dispatch) {
-  return {
-    // manual
-    // createCourse: (course) => dispatch(courseActions.createCourse(course)),
-    // courseActions contains all actions
-
-    // we can specify what actions to include
-    actions: {
-      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
-    }
-  };
-}
+// mapDispatchToProps can be written as object instead of function
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors
+};
 
 // connect returns a function, that function then calls our component
 // when we omit mapDispatchToProps our component gets a dispatch prop injected automatically
